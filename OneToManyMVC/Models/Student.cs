477655f1a238580504a43ac9dfa52c43ab1308e0
Artavazd Gevorgyan <ag15012003@gmail.com>
2019-02-02 15:51:45 +0400

    Added Student

---
+++ OneToManyMVC/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneToManyMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+
+        [ForeignKey("Grade")]
+        public int CurrentGradeID { get; set; }
+        [ForeignKey("Ambion")]
+        public int CurrentAmbionID { get; set; }
+        public Ambion Ambion { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
